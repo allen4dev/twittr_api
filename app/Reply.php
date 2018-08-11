c593265c7d63433b729faa93dac29de7ee7e4806
allen4dev <alanaliagadev@gmail.com>
2018-08-11 02:21:34 -0400

    [Update] Moved the hability to record an activity to his own trait RecordActivity

--- app/Reply.php
+++ app/Reply.php
@@ -4,26 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Traits\RecordActivity;
+
 class Reply extends Model
 {
+    use RecordActivity;
+
     protected $fillable = [ 'body', 'user_id', 'tweet_id' ];
-
-    protected static function boot()
-    {
-        if (auth()->guest()) return;
-
-        static::created(function ($model) {
-            $model
-                ->activity()
-                ->create([
-                    'user_id' => auth()->id(),
-                    'type'    => 'created_' . strtolower((new \ReflectionClass($model))->getShortName()),
-                ]);
-        });
-    }
-
-    public function activity()
-    {
-        return $this->morphMany(Activity::class, 'subject');
-    }
 }
--- app/Tweet.php
+++ app/Tweet.php
@@ -4,28 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Traits\RecordActivity;
+
 class Tweet extends Model
 {
+    use RecordActivity;
+
     protected $fillable = [ 'body', 'user_id' ];
-
-    protected static function boot()
-    {
-        if (auth()->guest()) return;
-
-        static::created(function ($model) {
-            $model
-                ->activity()
-                ->create([
-                    'user_id' => auth()->id(),
-                    'type'    => 'created_' . strtolower((new \ReflectionClass($model))->getShortName()),
-                ]);
-        });
-    }
-
-    public function activity()
-    {
-        return $this->morphMany(Activity::class, 'subject');
-    }
 
     public function replies()
     {
